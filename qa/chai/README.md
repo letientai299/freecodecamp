bb85cf5ae2e2db92606a909349ad16f819f3328e
Tai <letientai299@gmail.com>
2021-03-18 19:45:20 +0800

    Clone, reformat and done some challenge from FCC source

---
+++ qa/chai/README.md
@@ -0,0 +1,6 @@
+# Quality Assurance with Chaii
+
+- [FreeCodeCamp Challenge](https://www.freecodecamp.org/learn/quality-assurance/quality-assurance-and-testing-with-chai/)
+- [Original source](https://github.com/freeCodeCamp/boilerplate-mochachai/)
+
+This contains my modified code to pass the challenge.
---
+++ qa/chai/server.js
@@ -0,0 +1,119 @@
+"use strict";
+const express = require("express");
+const app = express();
+
+const cors = require("cors");
+const runner = require("./test-runner");
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/hello", function(req, res) {
+  const name = req.query.name || "Guest";
+  res.type("txt").send("hello " + name);
+});
+
+const travellers = function(req, res) {
+  let data = {};
+  if (req.body && req.body.surname) {
+    switch (req.body.surname.toLowerCase()) {
+      case "polo":
+        data = {
+          name: "Marco",
+          surname: "Polo",
+          dates: "1254 - 1324"
+        };
+        break;
+      case "colombo":
+        data = {
+          name: "Cristoforo",
+          surname: "Colombo",
+          dates: "1451 - 1506"
+        };
+        break;
+      case "vespucci":
+        data = {
+          name: "Amerigo",
+          surname: "Vespucci",
+          dates: "1454 - 1512"
+        };
+        break;
+      case "da verrazzano":
+      case "verrazzano":
+        data = {
+          name: "Giovanni",
+          surname: "da Verrazzano",
+          dates: "1485 - 1528"
+        };
+        break;
+      default:
+        data = {
+          name: "unknown"
+        };
+    }
+  }
+  res.json(data);
+};
+
+app.route("/travellers").put(travellers);
+
+let error;
+app.get(
+  "/_api/get-tests",
+  cors(),
+  function(req, res, next) {
+    if (error) return res.json({ status: "unavailable" });
+    next();
+  },
+  function(req, res, next) {
+    if (!runner.report) return next();
+    res.json(testFilter(runner.report, req.query.type, req.query.n));
+  },
+  function(req, res) {
+    runner.on("done", function(report) {
+      process.nextTick(() =>
+        res.json(testFilter(runner.report, req.query.type, req.query.n))
+      );
+    });
+  }
+);
+
+app.listen(3000, function() {
+  console.log("Listening on port " + 3000);
+  console.log("Running Tests...");
+  setTimeout(function() {
+    try {
+      runner.run();
+    } catch (e) {
+      error = e;
+      console.log("Tests are not valid:");
+      console.log(error);
+    }
+  }, 1500);
+});
+
+module.exports = app; // for testing
+
+function testFilter(tests, type, n) {
+  let out;
+  switch (type) {
+    case "unit":
+      out = tests.filter(t => t.context.match("Unit Tests"));
+      break;
+    case "functional":
+      out = tests.filter(t => t.context.match("Functional Tests"));
+      break;
+    default:
+      out = tests;
+  }
+  if (n !== undefined) {
+    return out[n] || out;
+  }
+  return out;
+}
