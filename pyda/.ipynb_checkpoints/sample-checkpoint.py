82c6ed106bce04163ce8516a6f113c45e6d5ea74
Tai <letientai299@gmail.com>
2021-03-25 01:48:48 +0800

    Remove dummy test file

---
+++ pyda/.ipynb_checkpoints/sample-checkpoint.py
@@ -0,0 +1,7 @@
+# +
+# A single code cell made of two paragraphs
+a = 1
+
+
+def f(x):
+    return x + a
---
+++ pyda/.ipynb_checkpoints/test-checkpoint.py
@@ -0,0 +1,15 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.11.0
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
