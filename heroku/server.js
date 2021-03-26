c6bbad9172e81c28728bc87d19e968b3dead1047
Tai <letientai299@gmail.com>
2021-03-27 02:46:36 +0800

    Disable live server if running on production

--- heroku/server.js
+++ heroku/server.js
@@ -1,29 +1,32 @@
 const express = require("express");
 const path = require("path");
-
-// see the guide for setting up live reload both front and back ends here.
-// https://bytearcher.com/articles/refresh-changes-browser-express-livereload-nodemon/
-const connectLivereload = require("connect-livereload");
-const livereload = require("livereload");
-
-// setup live reload server
-const liveReloadServer = livereload.createServer();
-let msPath = path.join(__dirname, "../microservices");
-liveReloadServer.watch(msPath);
-let qaPath = path.join(__dirname, "../qa");
-liveReloadServer.watch(qaPath);
-
-// sent reload refresh to client  on local files chance.
-liveReloadServer.server.once("connection", () => {
-  setTimeout(() => {
-    liveReloadServer.refresh("/");
-  }, 100);
-});
-
 const app = express();
 
-// inject client script to receive reload notification from live reload server.
-app.use(connectLivereload());
+const isProd = process.env.NODE_ENV !== undefined;
+
+if (!isProd) {
+  // see the guide for setting up live reload both front and back ends here.
+  // https://bytearcher.com/articles/refresh-changes-browser-express-livereload-nodemon/
+  const connectLivereload = require("connect-livereload");
+  const livereload = require("livereload");
+
+  // setup live reload server
+  const liveReloadServer = livereload.createServer();
+  let msPath = path.join(__dirname, "../microservices");
+  liveReloadServer.watch(msPath);
+  let qaPath = path.join(__dirname, "../qa");
+  liveReloadServer.watch(qaPath);
+
+  // sent reload refresh to client  on local files chance.
+  liveReloadServer.server.once("connection", () => {
+    setTimeout(() => {
+      liveReloadServer.refresh("/");
+    }, 100);
+  });
+
+  // inject client script to receive reload notification from live reload server.
+  app.use(connectLivereload());
+}
 
 let port = process.env.PORT;
 
