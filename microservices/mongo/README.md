cbaf17feb016916d4261d35ffe5f0824c3778d4b
Tai <letientai299@gmail.com>
2021-03-27 20:50:59 +0800

    Add sample code for mongo course

---
+++ microservices/mongo/README.md
@@ -0,0 +1,10 @@
+# Backend Challenges - Microservices course
+
+The code is copied and modified from [here][fcc_ms_gh] for the [MongoDB and
+Mongoose][fcc_ms] course.
+
+[fcc_ms_gh]: https://github.com/freeCodeCamp/boilerplate-mongomongoose
+[fcc_ms]:
+  https://www.freecodecamp.org/learn/apis-and-microservices/#mongodb-and-mongoose
+
+However, for this one, the heroku version
---
+++ microservices/mongo/myApp.js
@@ -0,0 +1,81 @@
+require("dotenv").config();
+
+const mockDB = require("mongo-mock");
+mockDB.max_delay = 0;
+const mockMongoClient = mockDB.MongoClient;
+mockMongoClient.persist = "mongo-mock.js";
+
+const mongoURL = "mongodb://localhost:27017/mongo";
+
+//persist the data to disk
+
+const mongoose = require("mongoose");
+// mongoose.connect("", { useNewUrlParser: true, useUnifiedTopology: true });
+
+let Person;
+
+const createAndSavePerson = (done) => {
+  done(null /*, data*/);
+};
+
+const createManyPeople = (arrayOfPeople, done) => {
+  done(null /*, data*/);
+};
+
+const findPeopleByName = (personName, done) => {
+  done(null /*, data*/);
+};
+
+const findOneByFood = (food, done) => {
+  done(null /*, data*/);
+};
+
+const findPersonById = (personId, done) => {
+  done(null /*, data*/);
+};
+
+const findEditThenSave = (personId, done) => {
+  const foodToAdd = "hamburger";
+
+  done(null /*, data*/);
+};
+
+const findAndUpdate = (personName, done) => {
+  const ageToSet = 20;
+
+  done(null /*, data*/);
+};
+
+const removeById = (personId, done) => {
+  done(null /*, data*/);
+};
+
+const removeManyPeople = (done) => {
+  const nameToRemove = "Mary";
+
+  done(null /*, data*/);
+};
+
+const queryChain = (done) => {
+  const foodToSearch = "burrito";
+
+  done(null /*, data*/);
+};
+
+/** **Well Done !!**
+ /* You completed these challenges, let's go celebrate !
+ */
+
+//----- **DO NOT EDIT BELOW THIS LINE** ----------------------------------
+
+exports.PersonModel = Person;
+exports.createAndSavePerson = createAndSavePerson;
+exports.findPeopleByName = findPeopleByName;
+exports.findOneByFood = findOneByFood;
+exports.findPersonById = findPersonById;
+exports.findEditThenSave = findEditThenSave;
+exports.findAndUpdate = findAndUpdate;
+exports.createManyPeople = createManyPeople;
+exports.removeById = removeById;
+exports.removeManyPeople = removeManyPeople;
+exports.queryChain = queryChain;
